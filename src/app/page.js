8b94321153a3f68e76aa598a799eebc142a0b809
H2Vid <dstmpl17@gmail.com>
2024-12-19 22:37:27 +0700

    install ulang nextJS serta third party ui.acertenity

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
 import AboutMe from "@/components/aboutSection/AboutMe"
-import Contact from "@/components/contact/contact"
 import Experience from "@/components/experience/Experience"
 import HeroSection from "@/components/heroSection/heroSection"
-import SkillAndTools from "@/components/skill/SkillAndTools"
+import { ToolSet } from "@/components/toolsSet/toolSet"
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <HeroSection />
-        <AboutMe />
-        <Experience />
-        <SkillAndTools />
-        <Contact />
-      </main>
-    </>
+    <main>
+      {/* <HeroSection /> */}
+      {/* <AboutMe /> */}
+      {/* <Experience /> */}
+      <ToolSet />
+    </main>
   )
 }
